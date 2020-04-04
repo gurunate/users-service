4d677cf490fd0797cb5e815638a608e381e37e3c
Nate Johnson <nathanalanjohnson@gmail.com>
2020-04-04 08:30:41 -0500

    WIP

---
+++ db/setup.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Jordane','Schumm','Samanta_Kshlerin@gmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Dessie','MacGyver','Roselyn62@gmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Reynold','Adams','Kiley_Willms80@gmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Raheem','Kuphal','Rosalind64@yahoo.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Mckenna','Runolfsson','Raymond65@gmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Johnson','Ziemann','Arianna.Ebert86@yahoo.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Tyler','Pacocha','Larissa22@hotmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Rafaela','Dickinson','Aurelio.Jacobs@hotmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Lourdes','Nikolaus','Garth.Lakin@hotmail.com',now(),now());
+INSERT INTO Users (firstName,lastName,email,createdAt,updatedAt) VALUES ('Conner','Prosacco','Lesly68@gmail.com',now(),now());
